648434da6d0532688edb5bb9bb77fad2aa8e5ef4
Guilherme Ataide <159018202+Guilherme0309@users.noreply.github.com>
2024-02-05 09:58:39 -0300

    Add files via upload

---
+++ frase.sh
@@ -0,0 +1,21 @@
+echo "vamos iniciar as atividades"
+
+sudo apt update;
+
+sudo apt upgrade;
+
+echo "agora vamos instalar o APACHE"
+
+sudo apt install mysql-server;
+
+sudo apt install php;
+
+sudo apt install phpmyadmin;
+
+sudo apt install apache2;
+
+#escolha usar o apachee configure o phmyadmin com um usuario e senha para o banco de dados
+
+#após isso abra o browser e teste : http://localhost/phpmyadmin
+
+sudo apt install  ssh;
